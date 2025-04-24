2802e4612867d8e139406c6435676e6d35638d83
juliatk <juia_atkeson25@milton.edu>
2025-04-24 09:25:30 -0400

    start

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Spotify API</h1>
+        </div>
+        <p>Retrieve metadata about artists, tracks, and playlists, create and manage playlists, control playback, and even analyze user listening data</p>
+    </>
+  
+  )
+}
+
+export default App
